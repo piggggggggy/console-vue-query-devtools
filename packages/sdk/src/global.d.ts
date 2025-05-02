f2fa7d12a43d97ba725ac64b4919bcca68718ac4
piggggggggy <samuel.park@mz.co.kr>
2025-05-03 00:06:13 +0900

    fix: edit event data type

--- packages/sdk/src/global.d.ts
+++ packages/sdk/src/global.d.ts
@@ -1,4 +1,4 @@
-import type { Query, QueryClient } from '@tanstack/vue-query';
+import type { QueryClient, QueryKey, QueryState } from '@tanstack/vue-query';
 
 declare global {
   interface Window {
@@ -11,17 +11,26 @@ export interface ConsoleVueQueryDevtoolsHook {
     getQueryClient: () => QueryClient | null;
 }
 
+export interface ConsoleQueryMessageData {
+    queryKey: QueryKey;
+    queryHash: string;
+    state: QueryState;
+    gcTime: number;
+    staleTime: number;
+    enabled: boolean;
+}
+
 export interface QueryDebugMessage {
     source: 'console-vue-query-devtools';
     type: 'QUERY_DEBUG_DATA';
-    queries: Query[];
+    queries: ConsoleQueryMessageData[];
 }
 
 export type DevToolsIncomingMessage = QueryDebugMessage;
 
 export interface DevToolsOutgoingMessage {
     type: 'QUERY_DEBUG_DATA';
-    queries: Query[];
+    queries: ConsoleQueryMessageData[];
 }
 
 export type MessageType = 'QUERY_DEBUG_DATA';
